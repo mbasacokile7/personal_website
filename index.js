3449f4dd9e00334acb50edc7421bd465280e7633
Mbasa <mbasacokile7@gmail.com>
2024-10-09 05:34:08 +0200

    Set Up particles background and Navbar structure first touches

---
+++ index.js
@@ -0,0 +1,11 @@
+// Initiate the particles background
+window.onload = function() {
+    Particles.init({
+      selector: '.background',
+      connectParticles: true,
+      color: '#FFFFFF',
+      maxParticles: 150,
+      sizeVariations: 4,
+    });
+  };
+  
